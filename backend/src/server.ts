a7c908a7985aa2f0e17746c9daccafbcf9dfef21
jhaytaslim <jhaytaslim@gmail.com>
2025-11-17 14:35:29 +0100

    chore: added cors url, all for now

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,11 @@ import { AppConfig } from "../config";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 // Swagger/OpenAPI setup (new)
